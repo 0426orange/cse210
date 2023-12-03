2d6f6b01964ba9c8471122e28e0149199e72d746
0426orange <114186672+0426orange@users.noreply.github.com>
2023-12-02 17:28:09 -0700

    Add files via upload

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    public string Reference { get; }
+    public string Text { get; }
+    private List<string> hiddenWords;
+
+    public Scripture(string reference, string text)
+    {
+        Reference = reference;
+        Text = text;
+        hiddenWords = Text.Split(' ').ToList();
+    }
+
+    public void HideWords(int count)
+    {
+        if (count >= hiddenWords.Count)
+        {
+            Console.WriteLine("All words are hidden. Memorization complete!");
+            return;
+        }
+
+        int wordsToHide = Math.Min(count, hiddenWords.Count);
+        int startIndex = hiddenWords.Count - wordsToHide;
+
+        for (int i = startIndex; i < hiddenWords.Count; i++)
+        {
+            hiddenWords[i] = new string('*', hiddenWords[i].Length);
+        }
+
+        DisplayScripture();
+    }
+
+    public void DisplayScripture()
+    {
+        Console.Clear();
+        Console.WriteLine($"{Reference}:");
+        Console.WriteLine(string.Join(" ", hiddenWords));
+        Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Example usage
+        Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
+        while (true)
+        {
+            scripture.DisplayScripture();
+
+            string input = Console.ReadLine().ToLower();
+            if (input == "quit")
+            {
+                break;
+            }
+            else if (input == "")
+            {
+                scripture.HideWords(2); // Adjust the number of words to hide
+            }
+        }
+
+        Console.WriteLine("Program ended.");
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Threading;
+
+public class MindfulnessProgram
+{
+    private int countdownSeconds = 5; // Default countdown time
+
+    public void StartBreathingActivity()
+    {
+        Console.WriteLine("Breathing Activity: Take a deep breath in and out.");
+        CountdownAnimation();
+    }
+
+    public void StartReflectionActivity()
+    {
+        Console.WriteLine("Reflection Activity: Think deeply about a meaningful experience.");
+        CountdownAnimation();
+    }
+
+    public void StartListingActivity()
+    {
+        Console.WriteLine("Listing Activity: List as many positive things as you can.");
+        CountdownAnimation();
+    }
+
+    private void CountdownAnimation()
+    {
+        Console.WriteLine($"Get ready... Starting in {countdownSeconds} seconds.");
+        for (int i = countdownSeconds; i > 0; i--)
+        {
+            Console.Write($"{i} ");
+            Thread.Sleep(1000); // Pause for 1 second
+        }
+        Console.WriteLine("\nActivity started!\n");
+        // Add logic for the respective activity here
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        MindfulnessProgram mindfulnessProgram = new MindfulnessProgram();
+
+        while (true)
+        {
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Exit");
+
+            Console.Write("Select an option (1-4): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    mindfulnessProgram.StartBreathingActivity();
+                    break;
+                case "2":
+                    mindfulnessProgram.StartReflectionActivity();
+                    break;
+                case "3":
+                    mindfulnessProgram.StartListingActivity();
+                    break;
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
+            }
+        }
     }
 }
